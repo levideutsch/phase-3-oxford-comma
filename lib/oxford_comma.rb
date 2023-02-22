02b921cb4e11fd4bb7cd912c15e4797fd6bf18a8
levideutsch <levideutsch@live.com>
2023-02-22 16:04:41 -0500

    commit tree efdaab72a59bee3aa8d2134fe043978357f54fbc

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
+require 'pry'
 def oxford_comma(array)
-
+    # if array.length <= 2
+    #     array.join(" and ")
+    # elsif array.length == 3
+    #     array[-1] = "and starfruit"
+    #     array.join(", ")
+    # elsif array.length > 3
+    #     array[-1] = "and dragon fruits" 
+    #     array.join(", ")
+    #  elsif array.length > 3 && array[-1] == "and dragon fruits" 
+    #      array[-1] = "and pomelos" 
+    #      array.join(", ")
+    #    binding.pry
+    # end
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        array[-1] = "and" + " " + array[-1]
+        array.join(", ")
+    end
 end
+
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])
